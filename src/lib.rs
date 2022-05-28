3ebee7f8ddaf1af189e85512f1212ff03e6009be
Stuart Terrett <shterrett@gmail.com>
2022-05-28 09:36:22 -0400

    Add let-in expressions

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,17 @@
 pub enum AST {
     ILit(i64),
     BLit(bool),
+    /// Variable Bindings; this holds the name of the variable
+    /// No attempt is made at renaming / deBruijn to prevent collisions for this exercise.
+    Var(String),
     Add(Box<AST>, Box<AST>),
     /// Greater than
     Gt(Box<AST>, Box<AST>),
     /// If expressions. Order of arguments is
     /// (test expression, expression if true, expression if false)
     If(Box<AST>, Box<AST>, Box<AST>),
+    /// Let varname = expression in body
+    Let(String, Box<AST>, Box<AST>),
 }
 
 /// Convenience constructor for AST::ILit
@@ -36,6 +41,15 @@ pub fn if_e(test: AST, if_true: AST, if_false: AST) -> AST {
     AST::If(Box::new(test), Box::new(if_true), Box::new(if_false))
 }
 
+/// Convenience constructor for AST::Let
+pub fn let_in(name: &str, bind: AST, body: AST) -> AST {
+    AST::Let(name.to_string(), Box::new(bind), Box::new(body))
+}
+
+pub fn var(name: &str) -> AST {
+    AST::Var(name.to_string())
+}
+
 /// The opcodes that will be emitted by the compiler and interpreted by the vm to execute the
 /// program
 #[derive(Debug, Eq, PartialEq)]
@@ -47,6 +61,12 @@ pub enum Opcode {
     /// Relative jump if the item on the top of the stack is `true`. This is used to implement if
     /// statements
     JumpIfTrue(usize),
+    /// Bind the current head of the stack to the given local variable name
+    BindLocal(String),
+    /// Unbind the local variable (and pop the value off the stack)
+    DropLocal,
+    /// Look up a local variable from the environment
+    LookupLocal(String),
     /// Primitive Add. Will add opcodes for primitive operations for now, and will remove and
     /// replace with functions if/when I get around to defining functions
     Add,
@@ -60,15 +80,23 @@ pub enum Term {
     Bool(bool),
 }
 
-/// A thin wrapper around a Vec just to allow pop to be re-implemented
+/// Wraps a vec that is used for the program stack and a mapping of local variable names to their
+/// positions on the stack
 #[derive(Debug, Eq, PartialEq)]
 pub struct Stack {
     stack: Vec<Term>,
+    /// Only one variable is bound in each let-in expression. Binding a variable will involve
+    /// pushing its name and the current tip of the stack onto `locals`. Dropping a binding will
+    /// simply pop the most recent local off the locals
+    locals: Vec<(String, usize)>,
 }
 
 impl Stack {
     pub fn new() -> Self {
-        Stack { stack: vec![] }
+        Stack {
+            stack: vec![],
+            locals: vec![],
+        }
     }
 
     fn push(&mut self, t: Term) {
@@ -82,6 +110,33 @@ impl Stack {
             .pop()
             .expect("Error -- pop called on empty stack. This should not be possible")
     }
+
+    /// BindLocal is called directly after the bind expression in a let-in, so the local variable
+    /// is the current head of the stack
+    fn bind_local(&mut self, v: String) {
+        let sp = self.stack.len() - 1;
+        self.locals.push((v, sp));
+    }
+
+    /// DropLocal is called directly after the body expression in a let-in, so the stack looks like
+    /// `[..., localvar, bodyresult]`
+    fn drop_local(&mut self) {
+        self.locals
+            .pop()
+            .expect("Error -- tried to drop an unbound local");
+        let result = self.pop(); // result of body
+        self.pop(); // local variable
+        self.push(result);
+    }
+
+    fn lookup(&self, name: &str) -> Term {
+        let (_, pos) = self
+            .locals
+            .iter()
+            .rfind(|(binding, _)| binding == name)
+            .expect(&format!("Error -- unbound variable {}", name));
+        self.stack[*pos].clone()
+    }
 }
 
 /// This compiles the AST into a Vec of Opcodes that can be executed by the vm
@@ -91,6 +146,7 @@ pub fn compile(ast: AST) -> Vec<Opcode> {
     match ast {
         AST::ILit(i) => vec![Opcode::LoadConst(Term::Int(i))],
         AST::BLit(i) => vec![Opcode::LoadConst(Term::Bool(i))],
+        AST::Var(s) => vec![Opcode::LookupLocal(s)],
         AST::Add(x, y) => {
             let mut left = compile(*x);
             let mut right = compile(*y);
@@ -124,6 +180,17 @@ pub fn compile(ast: AST) -> Vec<Opcode> {
             test.push(Opcode::Jump(false_instrs + 1)); // jump over the false section if the true section executed plue one to get past the last instruction of false
             test.append(&mut if_false); // jump into the false section if the true section is skipped
             test
+        }
+        AST::Let(v, x, y) => {
+            let mut bind = compile(*x);
+            let mut body = compile(*y);
+            // Evaluate the binding. Then bind the variable so it can be referenced in the body.
+            // After the body has been evaluated, drop the binding so the variable is no longer in
+            // scope.
+            bind.push(Opcode::BindLocal(v));
+            bind.append(&mut body);
+            bind.push(Opcode::DropLocal);
+            bind
         }
     }
 }
@@ -170,6 +237,21 @@ pub fn run(program: Vec<Opcode>, stack: &mut Stack) -> Term {
             }
             Opcode::Jump(n) => {
                 pc += n;
+            }
+            Opcode::BindLocal(s) => {
+                stack.bind_local(s.clone());
+                pc += 1;
+            }
+            Opcode::DropLocal => {
+                stack.drop_local();
+                pc += 1;
+            }
+            Opcode::LookupLocal(name) => {
+                // The variable is looked up and cloned, and then pushed onto the top of the stack
+                // to be referenced by the next operation
+                let l = stack.lookup(name);
+                stack.push(l);
+                pc += 1;
             }
         }
     }
@@ -260,5 +342,40 @@ mod tests {
         let c = compile(program);
         let result = run(c, &mut stack);
         assert_eq!(result, Term::Int(0));
+    }
+
+    #[test]
+    fn local_add() {
+        let mut stack = Stack::new();
+        let program = let_in("x", int_lit(5), add(int_lit(1), var("x")));
+        let c = compile(program);
+        let result = run(c, &mut stack);
+        assert_eq!(result, Term::Int(6));
+    }
+
+    #[test]
+    fn nested_let() {
+        let mut stack = Stack::new();
+        let program = let_in(
+            "x",
+            int_lit(5),
+            let_in("y", int_lit(1), add(var("y"), var("x"))),
+        );
+        let c = compile(program);
+        let result = run(c, &mut stack);
+        assert_eq!(result, Term::Int(6));
+    }
+
+    #[test]
+    fn surrounded_let() {
+        let mut stack = Stack::new();
+        let program = let_in(
+            "x",
+            int_lit(3),
+            add(let_in("y", int_lit(1), add(var("y"), var("x"))), int_lit(2)),
+        );
+        let c = compile(program);
+        let result = run(c, &mut stack);
+        assert_eq!(result, Term::Int(6));
     }
 }
